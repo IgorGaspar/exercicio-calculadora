4966b1eec76d0d8c9c5cb848e8b3a32eb1fb19a8
Igor Gaspar <igor.elvis@hotmail.com>
2020-04-06 14:51:14 -0300

    Programa que calcula a circunferencia e o volume de um diametro a partir do raio

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercícioCalculadora {
+    class Program {
+        
+        static void Main(string[] args) {
+
+            /* Programa que realiza o cálculo da circunferência
+            e do volume de um diâmetro a partir do valor do 
+            raio do mesmo. */
+            
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
